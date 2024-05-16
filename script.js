73a2bee3ba6f425522a2af3b80547da8bfc26240
ZakariaAdam2225 <168724101+ZakariaAdam2225@users.noreply.github.com>
2024-05-16 12:05:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
